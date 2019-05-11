9803896a056623bb4ece633b0e0e177ca02799c7
Blizzzer <k.apolinarski@gmail.com>
2019-05-11 13:04:52 +0200

    Initial commit

---
+++ src/main/java/com/codeknab/sportgeeks/SportgeeksApplication.java
@@ -0,0 +1,13 @@
+package com.codeknab.sportgeeks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportgeeksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportgeeksApplication.class, args);
+    }
+
+}

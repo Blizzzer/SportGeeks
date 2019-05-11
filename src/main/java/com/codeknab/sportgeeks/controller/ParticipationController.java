9b620427d165a7e7fe7df464f791ec663f2fb2ed
Blizzzer <k.apolinarski@gmail.com>
2019-05-11 19:59:54 +0200

    problem solved with participation add response

--- src/main/java/com/codeknab/sportgeeks/controller/ParticipationController.java
+++ src/main/java/com/codeknab/sportgeeks/controller/ParticipationController.java
@@ -1,18 +1,13 @@
 package com.codeknab.sportgeeks.controller;
 
 import com.codeknab.sportgeeks.domain.Participation;
-import com.codeknab.sportgeeks.dtos.ParticipationDTO;
 import com.codeknab.sportgeeks.dtos.ParticipationPostDTO;
 import com.codeknab.sportgeeks.mapper.ParticipationMapper;
 import com.codeknab.sportgeeks.service.ParticipationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping("/participation")
 public class ParticipationController {
     @Autowired
@@ -26,8 +21,6 @@ public class ParticipationController {
                                                          @PathVariable Long eventId) {
         Participation participationToAdd = mapper.toParticipation(participation);
         participationToAdd.setSportEventId(eventId);
-        Participation savedParticipation = service.addParticipation(participationToAdd);
-        ParticipationPostDTO participationPostDTOTOReturn = mapper.toParticipationPostDTO(savedParticipation);
-        return participationPostDTOTOReturn;
+        return mapper.toParticipationPostDTO(service.addParticipation(participationToAdd));
     }
 }
